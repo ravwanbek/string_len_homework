32ace6875e253be8a9da24804c65cc0bb4270f21
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-06 02:06:51 +0900

    7

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,30 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a=len(s1)
+    b=len(s2)
+    c=len(s3)
+    if a%2!=0 and b%2==0 and c%2==0:
+        answer=[s1]
+    elif b%2!=0 and a%2==0 and c%2==0:
+        answer=[s2]
+    elif c%2!=0 and a%2==0 and b%2==0:
+        answer=[s3]
+    elif a%2!=0 and b%2!=0 and c%2==0:
+        answer=[s1,s2]
+    elif a%2!=0 and c%2!=0 and b%2==0:
+        answer=[s1,s3]
+    elif b%2!=0 and c%2!=0 and a%2==0:
+        answer=[s2,s3]
+    elif a%2!=0 and b%2!=0 and c%2!=0:
+        answer=[s1,s2,s3]
+    
+    return answer   
+print(main(s1='Kia',s2='Toyota',s3='BMW'))
+
+
+        
+
+
+
+    
