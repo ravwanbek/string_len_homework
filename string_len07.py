cc6847046cdeaf405492ce3bd9190a3138a115de
ravwanbek <90487558+ravwanbek@users.noreply.github.com>
2022-01-08 02:07:02 +0900

    7

--- string_len07.py
+++ string_len07.py
@@ -1,36 +1,39 @@
 def main(s1,s2,s3):
     """
-    Given three strings, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
+    Given three ngs, s1, s2 and s3. return their odd lengths, example "[s1, s2]". If there is no odd length, return "[]".
     Args:
-        s1: string
-        s2: string
-        s3: string
+        s1: ng
+        s2: ng
+        s3: ng
     Returns:
         string
     """
+    s1=str(s1)
+    s2=str(s2)
+    s3=str(s3)
     a=len(s1)
     b=len(s2)
     c=len(s3)
 
     if a%2!=0 and b%2==0 and c%2==0:
-        answer='['+s1+']'
+        answer=f'{s1}'
     elif b%2!=0 and a%2==0 and c%2==0:
-        answer='['+s2+']'
+        answer=f'{s2}'
     elif c%2!=0 and a%2==0 and b%2==0:
-        answer='['+s3+']'
+        answer=f'{s3}'
     elif a%2!=0 and b%2!=0 and c%2==0:
-        answer='['+s1+','+s2+']'
+        answer='['+f'{s1}'+','+' '+f'{s2}'+']'
     elif a%2!=0 and c%2!=0 and b%2==0:
-        answer='['+s1+','+s3+']'
+        answer='['+f'{s1}'+','+' '+f'{s3}'+']'
     elif b%2!=0 and c%2!=0 and a%2==0:
-        answer='['+s2+','+s3+']'
+        answer='['+f'{s2}'+','+' '+f'{s3}'+']'
     elif a%2!=0 and b%2!=0 and c%2!=0:
-        answer='['+s1+','+s2+','+s3+']'
+        answer='['+f'{s1}'+','+' '+f'{s2}'+','+' '+f'{s3}'+']'
     elif a%2==0 and b%2==0 and c%2==0:
-        answer='[]'
+        answer=[]
     
     return answer   
-print(main(s1='kiaa',s2='hyundais',s3='volvof'))
+print(main('kia','hyundai','audi'))
 
 
         
